f45dbea9e0451227b581d617c8a792a9bc9347bd
kingzain08 <zai.ads008@gmail.com>
2020-09-16 13:55:50 +0700

    add function

--- src/wa_twister/support.php
+++ src/wa_twister/support.php
@@ -1,5 +1,15 @@
 <?php
 
+if( !function_exists('dd') )
+{
+    function dd()
+    {
+        echo "<pre>";
+        print_r(func_get_args());
+        exit();       
+    }    
+}
+
 function test_wa_twister()
 {
     return "okeeeeeee works!";
